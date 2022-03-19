fdd9c10dbadf1a47b7c6162db55699ee36594a40
Isabelle Prasad <iprasad1@my.bcit.ca>
2022-03-19 11:32:45 -0700

    final commit

--- ChatApp/server.js
+++ ChatApp/server.js
@@ -1,6 +1,76 @@
+const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const formatMessage = require('./utils/messages');
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers
+} = require('./utils/users');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'ChatApp - ';
+
+// Run when client connects
+io.on('connection', socket => {
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    // Welcome user
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+    // Broadcast when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // Send users and room info
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room)
+    });
+  });
+
+  // Listen for chatMessage
+  socket.on('chatMessage', msg => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg));
+  });
+
+  // Runs when client disconnects
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+
+      // Send users and room info
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room)
+      });
+    }
+  });
+});
+
 
 const PORT = 3000 || process.env.PORT
 
-app.listen(PORT);
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # WebDev_ExtraMarks
-A collaborative chat application made using Node, Express and Socket.io
+A collaborative chat application made using Node, Express and Socket.io.
+
+1. cd ChatApp
+2. npm install
+3. npm run dev
+    a. open 2 tabs with local host
+    b. sign in as 2 different users
+    c. send messages back and forth
+    d. have fun!
